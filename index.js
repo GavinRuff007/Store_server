02638ccf375a77af7b7f74e57757bd38069d1314
GavinRuff007 <134914088+GavinRuff007@users.noreply.github.com>
2023-10-13 09:25:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+const express = require('express');
+const app = express();
+const path = require('path');
+const https = require('https');
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+// Serve images from a directory
+const imageDirectory = path.join(__dirname, 'images');
+app.use('/images', express.static(imageDirectory));
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
+// Handle GET requests with the ibb.co URLs
+app.get('/:ibbUrl', (req, res) => {
+  // Extract the ibb.co URL from the request parameters
+  const ibbUrl = req.params.ibbUrl;
 
-const PORT = 8000
+  // Make a request to the ibb.co URL
+  https.get(ibbUrl, (ibbResponse) => {
+    if (ibbResponse.statusCode === 200) {
+      // Assuming the ibb.co URL serves an image, set the appropriate content type
+      res.set('Content-Type', 'image/jpeg');
+      // Pipe the ibb.co response to the client's response
+      ibbResponse.pipe(res);
+    } else {
+      // Handle error responses from ibb.co
+      res.status(ibbResponse.statusCode).send('Error accessing the ibb.co URL');
+    }
+  });
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+  // Handle errors in making the ibb.co request
+  https.on('error', (e) => {
+    res.status(500).send('Error making the request to ibb.co');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
